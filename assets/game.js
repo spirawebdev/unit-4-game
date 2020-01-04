136db3d5299a82449d77b3afd2a8d72dd8845e4b
Anthony Dees <spirawebdev@gmail.com>
2020-01-04 16:39:09 -0700

    Finished Crystal Collector Game!

--- assets/game.js
+++ assets/game.js
@@ -1,79 +1,99 @@
-var wins = 0;
-var losses = 0;
+var wins = 1;
+var losses = 1;
 var totalWins = 0;
-var playerCount = 0
-
-
-
-
-// Generate a random number and who it to user (range 19-120)//
+var playerCount = 0;
+// Generate a random number and who it to user (range 19-120)
 function randomNumber(min, max) {
     var randNumb = Math.floor(Math.random() * (max - min + 1)) + min;
     return randNumb;
 
 }
-
 var winningNumber = randomNumber(19, 120);
 console.log(winningNumber);
-
-
 // Create a .onclick function that adds a number to the player score for each button.
 
 
 $( document ).ready(function() {
     
 
-
 $('#red').on('click', function() {
-    playerCount = playerCount + 2; 
+    playerCount = playerCount + Math.floor(Math.random() * (12 - 1 + 1)) + 1;  
     $('#winNumb').text("Your number is " + playerCount);
+        if (playerCount === winningNumber){
+        alert("PERFECT!");
+        $('#loss').text("L: ");
+        $('#wins').text("W: " + wins++);
+        winningNumber = randomNumber(19, 120);
+    }
+        else if (playerCount > winningNumber) {
+        $('#loss').text("L: " + losses++);
+        playerCount = 0;
+    }
+
     return playerCount;
     
   });
 
   $('#blue').on('click', function() {
-    playerCount = playerCount + 5; 
+    playerCount = playerCount + Math.floor(Math.random() * (12 - 1 + 1)) + 1; 
     $('#winNumb').text("Your number is " + playerCount);
+        if (playerCount === winningNumber){
+        alert("PERFECT!");
+        $('#loss').text("L: ");
+        $('#wins').text("W: " + wins++);
+        winningNumber = randomNumber(19, 120);
+    }
+        else if (playerCount > winningNumber) {
+        $('#loss').text("L: " + losses++);
+        playerCount = 0;
+    }
     return playerCount;
     
   });
 
   $('#green').on('click', function() {
-    playerCount = playerCount + 10; 
+    playerCount = playerCount + Math.floor(Math.random() * (12 - 1 + 1)) + 1;  
     $('#winNumb').text("Your number is " + playerCount);
+        if (playerCount === winningNumber){
+        alert("PERFECT!");
+        $('#loss').text("L: ");
+        $('#wins').text("W: " + wins++);
+        winningNumber = randomNumber(19, 120);
+    }
+        else if (playerCount > winningNumber) {
+        $('#loss').text("L: " + losses++);
+        playerCount = 0;
+    }
     return playerCount;
     
   });
 
   $('#yellow').on('click', function() {
-    playerCount = playerCount + 25; 
+    playerCount = playerCount + Math.floor(Math.random() * (12 - 1 + 1)) + 1;  
     $('#winNumb').text("Your number is " + playerCount);
+        if (playerCount === winningNumber){
+        alert("PERFECT!");
+        $('#loss').text("L: ");
+        $('#wins').text("W: " + wins++);
+        winningNumber = randomNumber(19, 120);
+    }
+        else if (playerCount > winningNumber) {
+        $('#loss').text("L: " + losses++ );
+        playerCount = 0;
+    }
     return playerCount;
     
   });
 });
+//Update the Player score
+
+
+// Create an if statement that determines if the player has won the game or lost
 
 
 
+//Reset the game but somehow keep the players total wins
 
-//Update the Player score//
-
-
-
-
-
-
-
-// Create an if statement that determines if the player has won the game or lost //
-
-if (playerCount > winningNumber) {
-    $("#losses") = losses +1;
+function reset() {
+    playerCount = 0;
 }
-
-else if (playerCount === winningNumber) {
-    $("#wins").text("Wins: " + 1); 
-    $("wintotal") + 1;
-}
-
-
-//Reset the game but somehow keep the players total wins//
