b2b0bdbf73b3fa29382b91621f105a7ff7a97083
Anthony Dees <spirawebdev@gmail.com>
2020-01-04 04:47:50 -0700

    Functional javascript, but I need to add The W/L counter and a reset function

--- assets/game.js
+++ assets/game.js
@@ -19,12 +19,39 @@ console.log(winningNumber);
 
 // Create a .onclick function that adds a number to the player score for each button.
 
-$("#redButton").on("click", function() {
-    playerCount++;
-    alert("The counter after the red button was pushed is " + playerCount);
-    console.log(playerCount);
-});
 
+$( document ).ready(function() {
+    
+
+
+$('#red').on('click', function() {
+    playerCount = playerCount + 2; 
+    $('#winNumb').text("Your number is " + playerCount);
+    return playerCount;
+    
+  });
+
+  $('#blue').on('click', function() {
+    playerCount = playerCount + 5; 
+    $('#winNumb').text("Your number is " + playerCount);
+    return playerCount;
+    
+  });
+
+  $('#green').on('click', function() {
+    playerCount = playerCount + 10; 
+    $('#winNumb').text("Your number is " + playerCount);
+    return playerCount;
+    
+  });
+
+  $('#yellow').on('click', function() {
+    playerCount = playerCount + 25; 
+    $('#winNumb').text("Your number is " + playerCount);
+    return playerCount;
+    
+  });
+});
 
 
 
@@ -39,7 +66,14 @@ $("#redButton").on("click", function() {
 
 // Create an if statement that determines if the player has won the game or lost //
 
+if (playerCount > winningNumber) {
+    $("#losses") = losses +1;
+}
 
+else if (playerCount === winningNumber) {
+    $("#wins").text("Wins: " + 1); 
+    $("wintotal") + 1;
+}
 
 
 //Reset the game but somehow keep the players total wins//
